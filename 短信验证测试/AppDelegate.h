1c12a63f6010d4b265e64587b960178a6aab8d2e
纵昂 <zongang@zongangdemacbook-pro.local>
2017-02-13 13:53:29 +0800

    Initial Commit

---
+++ 短信验证测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  短信验证测试
+//
+//  Created by 纵昂 on 2017/2/13.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
